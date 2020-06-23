28ac4c803e13802ffaf42e7c45701f88bf3c89c9
mostafa <mostafaahmadi462@gmail.com>
2020-06-23 03:35:33 -0400

    solve small problem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,7 @@ public class Main {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            telegramBotsApi.registerBot(new ChannelHandlers());
-            telegramBotsApi.registerBot(new DirectionsHandlers());
-            telegramBotsApi.registerBot(new RaeHandlers());
-            telegramBotsApi.registerBot(new WeatherHandlers());
-            telegramBotsApi.registerBot(new TransifexHandlers());
-            telegramBotsApi.registerBot(new FilesHandlers());
+            telegramBotsApi.registerBot(new Bot462());
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
